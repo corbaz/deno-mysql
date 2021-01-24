b4806cfc10fe715314a59367f3acfa00719120f6
corbaz <julio.corbaz@gmail.com>
2021-01-24 08:12:14 -0300

    version 2

--- git-push.bat
+++ git-push.bat
@@ -1,9 +1,9 @@
 git add .
 @echo off
-set /P IP=Ingrese un commit: 
+set /P mensaje=Ingrese un commit: 
 
-echo "%IP%"
-git commit -m "%IP%"
+echo "%mensaje%"
+git commit -m "%mensaje%"
 git branch -M main
 rem git remote add origin https://github.com/corbaz/deno-mysql.git
 git push -u origin main
