b0702b689f9719ec0ca7775244b7f0be640149c2
corbaz <julio.corbaz@gmail.com>
2021-01-24 08:09:41 -0300

    'version2'

--- git-push.bat
+++ git-push.bat
@@ -1,5 +1,9 @@
 git add .
-git commit -m "version-04"
+@echo off
+set /P IP=Ingrese un commit: 
+
+echo '%IP%'
+git commit -m '%IP%'
 git branch -M main
 rem git remote add origin https://github.com/corbaz/deno-mysql.git
 git push -u origin main
---
+++ jcc.bat
@@ -0,0 +1,11 @@
+@echo off
+set /P IP=Ingrese su IP: 
+
+echo '%IP%'
+echo Hola Mundo
+pause
+rem exit
+set Nombre= Javier Letelier Ruiz
+echo %Nombre%
+
+pause
